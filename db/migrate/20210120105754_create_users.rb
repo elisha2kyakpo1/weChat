ba197a8c9ed16225b21cc63ccba3f657c391f9d9
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-20 13:12:45 +0200

    add controllers and models

---
+++ db/migrate/20210120105754_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.boolean :user_role
+
+      t.timestamps
+    end
+  end
+end
